15ade64376aa565a79d64c6cefff5b3f16e79d38
liubao <bao.liu@huawei.com>
2019-02-12 16:22:45 +0800

    Edge Service提供静态页面支持

--- porter/porter_lightweight/gateway-service/src/main/java/com/huawei/cse/porter/gateway/StaticWebpageDispatcher.java
+++ porter/porter_lightweight/gateway-service/src/main/java/com/huawei/cse/porter/gateway/StaticWebpageDispatcher.java
@@ -4,23 +4,29 @@ import org.apache.servicecomb.transport.rest.vertx.VertxHttpDispatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.netflix.config.DynamicPropertyFactory;
+
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 
-public class WebpageDispatcher implements VertxHttpDispatcher {
-  private static final Logger LOGGER = LoggerFactory.getLogger(WebpageDispatcher.class);
-  
-  
+public class StaticWebpageDispatcher implements VertxHttpDispatcher {
+  private static final Logger LOGGER = LoggerFactory.getLogger(StaticWebpageDispatcher.class);
+
+  private static final String WEB_ROOT = DynamicPropertyFactory.getInstance()
+      .getStringProperty("gateway.webroot", "/var/static")
+      .get();
+
   @Override
   public int getOrder() {
-    return 0;
+    return Integer.MAX_VALUE;
   }
 
   @Override
   public void init(Router router) {
-    String regex = "/ui/([^\\/]+)/(.*)";
+    String regex = "/ui/(.*)";
     StaticHandler webpageHandler = StaticHandler.create();
-    webpageHandler.setWebRoot("/static");
+    webpageHandler.setWebRoot(WEB_ROOT);
+    LOGGER.info("server static web page for WEB_ROOT={}", WEB_ROOT);
     router.routeWithRegex(regex).failureHandler((context) -> {
       LOGGER.error("", context.failure());
     }).handler(webpageHandler);
