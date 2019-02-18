04070c69a4e07b2996f8dedf4da093e774036b94
liubao <bao.liu@huawei.com>
2019-02-18 20:41:41 +0800

    Add Spring Boot 2 DEMO

--- porter/README.md
+++ porter/README.md
@@ -1,1 +1,11 @@
 这个项目帮助开发者学习如何使用CSE开发完整的微服务。 对应的指导文档参考：https://huaweicse.github.io/cse-java-chassis-doc/featured-topics/develop-microservice-using-cse.html
+
+# 子项目介绍
+
+* porter_lightweight
+最轻量级、运行最高效的方式实现Porter应用。静态页面采用js + html + css直接托管到gateway-service。file-service和user-service均采用轻量级Vert.x HTTP服务。
+
+* porter_springboot2
+gateway-service、file-service和user-service均采用Spring Boot打包运行。其中gateway-service的Web容器依然采用Vert.x HTTP服务， file-service和user-service采用Spring Boot提供的Web容器(Embedded Tomcat)。 
+
+
---
+++ porter/porter_springboot2/gateway-service/src/main/resources/ui/js/login.js
@@ -0,0 +1,35 @@
+function loginAction() {
+     var username = document.getElementById("username").value;
+     var password = document.getElementById("paasword").value;
+     var formData = {};
+     formData.userName = username;
+     formData.password = password;
+
+     $.ajax({
+        type: 'POST',
+        url: "/api/user-service/v1/user/login",
+        data: formData,
+        success: function (data) {
+            console.log(data);
+            setCookie("session-id", data.sessiondId, 1);
+            window.location = "/ui/upload.html";
+        },
+        error: function(data) {
+            console.log(data);
+            var error = document.getElementById("error");
+            error.textContent="登录失败";
+            error.hidden=false;
+        },
+        async: true
+    });
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
