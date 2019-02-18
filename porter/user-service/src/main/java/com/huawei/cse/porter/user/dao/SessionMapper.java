395813e65a3d1db1b7482732f6751aedcd0f724d
liubao <bao.liu@huawei.com>
2019-02-18 17:43:27 +0800

    删除老项目

--- porter/user-service/src/main/java/com/huawei/cse/porter/user/dao/SessionMapper.java
+++
@@ -1,9 +0,0 @@
-package com.huawei.cse.porter.user.dao;
-
-public interface SessionMapper {
-    void createSession(SessionInfo sessionInfo);
-
-    SessionInfo getSessioinInfo(String sessionId);
-    
-    void updateSessionInfo(String sessionId);
-}
--- porter/user-service/src/main/java/com/huawei/cse/porter/user/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.huawei.cse.porter.user.dao;
-
-public interface UserMapper {
-    void createUser(UserInfo userInfo);
-
-    UserInfo getUserInfo(String userName);
-}
